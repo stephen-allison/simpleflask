beef03fd1bc5c5b47fc0450989ba556795488a54
Stephen Allison <git@steve.allison.clara.co.uk>
2019-10-05 17:25:44 +0100

    move routes out of app.py which should be only for openshift entry point

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return "Hello, world from root app.py"
-
+import simpleflask.web as web
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    web.app.run(host='0.0.0.0', port=8080)
--- simpleflask/web.py
+++ simpleflask/web.py
@@ -5,8 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, world"
+    return "Hello, world from web.py"
 
-
-if __name__ == '__main__':
-    app.run()
